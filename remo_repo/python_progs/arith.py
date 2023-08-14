74a8ca3c48869debc5fe1948298bf7982a5776be
Ranjan.B <rockytomy20>
2023-08-14 16:10:09 +0530

    subtraction added to the file

--- remo_repo/python_progs/arith.py
+++ remo_repo/python_progs/arith.py
@@ -9,3 +9,11 @@ def divi(n1,n2):
 a=int(input('enter num1:'))
 b=int(input('enter num2:'))
 print('sum=',divi(a,b))
+
+
+def sub(n1,n2):
+    return(n1-n2)
+a=int(input('enter num1:'))
+b=int(input('enter num2:'))
+print('sum=',sub(a,b))
+
